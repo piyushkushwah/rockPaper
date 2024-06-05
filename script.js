2cac8db954e71ee4ee148d9163615a1949cc9cf6
Piyush Kushwaha <33251322+piyushkushwah@users.noreply.github.com>
2024-06-05 16:37:13 +0530

    Add files via upload
    UI design and functionality changes

--- script.js
+++ script.js
@@ -1,117 +1,188 @@
-let elements = ['rock', 'paper', 'scissor'];
-let score = [{user:{name:'test',points:0}},{cpu:{name:'test',points:0}}];
-let rules = ['rock > scissor', 'paper > rock', 'scissor > paper'];
-let elements_images = ['./assets/icon-rock.svg','./assets/icon-paper.svg','./assets/icon-scissors.svg'];
+const elements = ["rock", "paper", "scissor"];
+const borderColors = [
+    { name: "rock", color: "hsl(349, 71%, 52%)", shadowColor: '#9e152e' },
+    { name: "paper", color: "hsl(230, 89%, 62%)", shadowColor: '#2740be' },
+    { name: "scissor", color: "hsl(39, 89%, 49%)", shadowColor: '#c96d1d' },
+];
+let score = {
+    user: { name: "test", points: 0, totalScore: 0 },
+    cpu: { name: "test", points: 0, totalScore: 0 },
+};
+const rules = ["rock > scissor", "paper > rock", "scissor > paper"];
+const elements_images = [
+    "./assets/icon-rock.svg",
+    "./assets/icon-paper.svg",
+    "./assets/icon-scissors.svg",
+];
+let winnerName = "";
+let count = 0;
+const popupWrapper = document.getElementById('popupWrapper');
+const userScore = document.getElementById("userScore");
+const selectUserInputElement = document.getElementById("selectUserInputElement");
+const displayResult = document.getElementById("displayResult");
 
-
-let check = (str,userInput,cpuInput) => {
-
-        switch (str) {
-            case rules[0]:
-                calculatePoints(userInput, cpuInput, 'rock');
-                break;
-            case rules[1]:
-                console.log(userInput);
-                calculatePoints(userInput, cpuInput, 'paper');
-                break;
-            case rules[2]:
-                calculatePoints(userInput, cpuInput, 'scissor');
-                break;
-        }
-    
+function checkRules(str, userInput, cpuInput) {
+    switch (str) {
+        case rules[0]:
+            calculatePoints(userInput, cpuInput, "rock");
+            break;
+        case rules[1]:
+            calculatePoints(userInput, cpuInput, "paper");
+            break;
+        case rules[2]:
+            calculatePoints(userInput, cpuInput, "scissor");
+            break;
+    }
 }
 
-
-async function inputs(userInput) {
-
+function userInput(userInput) {
     let index = Math.round(Math.random() * (elements.length - 1));
     let cpuInput = elements[index];
-    
 
+    selectUserInputElement.style.display = "none";
+    displayResult.style.display = "flex";
 
-    document.getElementById('select_element').style.display = 'none';
-
-    document.getElementById('display_result')
-        .innerHTML = `   
+    displayResult.innerHTML = `
             <div  class="circle_wrap ">
-                <h3>You Picked</h3>
-                <div class="circle1 single_circle">
-                    <div class="circle_white single_white_circle">
+                <h3>YOU PICKED</h3>
+                <div class="user-input-wrapper">
+                <div id="userWinShowCircleBg" class="hide">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div style="border-color:${borderColors[userInput].color};
+                 box-shadow: 0 0.6em 0 ${borderColors[userInput].shadowColor}"
+                 class="circle single_circle">
+                    <div class="circle_white-border single_white_circle">
                         <img src="${elements_images[userInput]}" alt="">
                     </div>
                 </div>
+                </div>
+               
             </div> 
 
+            <div id="declareWinnerName">
+                <h1 style="visibility:hidden">Placeholder</h1>
+                <button class="button" onclick="playAgain()">Play Again</button>
+            </div>
+
+
             <div class="circle_wrap">
-                <h3>CPU Picked</h3>
-                <div id="circle2Result" class="circle1 single_circle" style="border:none!important">
-                    <div class="circle_white single_white_circle" style="display:none">
-                        <img src="${elements_images[index]}" alt="">
+                <h3>THE HOUSE PICKED</h3>
+                <div class="user-input-wrapper">
+                    <div id="cpuWinShowCircleBg" class="hide">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div style="border-color:${borderColors[index].color};
+                     box-shadow: 0 0.6em 0 ${borderColors[index].shadowColor}"
+                     id="circle2Result" class="circle single_circle">
+                        <div class="circle_white-border single_white_circle" style="display:none">
+                            <img src="${elements_images[index]}" alt="">
+                        </div>
                     </div>
                 </div>
             </div> 
-            `
-    
-    setTimeout( async () => {
-        
-        console.log();
+            `;
 
-        document.querySelector('#circle2Result').style.border = 'unset';
-        document.querySelector('#circle2Result .circle_white').style.display = 'flex';
-
-        setTimeout( async () => {
-                    
-            await checkIfEqual(elements[userInput],cpuInput);
-    
-            declareResult();
-
-        },100);
-        
-    },1000);
-  
-
-
+    setTimeout(() => {
+        document.querySelector(
+            "#circle2Result .circle_white-border"
+        ).style.display = "flex";
+        checkDueMatch(elements[userInput], cpuInput);
+    }, 100);
 }
 
 function calculatePoints(userInput, cpuInput, element) {
-    
     switch (true) {
         case cpuInput === element:
-            score[1].cpu.points += 1;    
+            score.cpu.points += 1;
             break;
         case userInput === element:
-            score[0].user.points += 1;    
+            score.user.points += 1;
             break;
     }
-
 }
-
 
 function declareResult() {
 
-    console.log(score);
-
-    if (score[0].user.points > score[1].cpu.points) {
-        score[0].user.points = 0;
-        alert('User Wins');
-    } else if (score[0].user.points < score[1].cpu.points){
-        score[1].cpu.points = 0;
-        alert('CPU Wins');
+    if (score.user.points > score.cpu.points) {
+        score.user.totalScore += 1;
+        userScore.innerText = score.user.totalScore;
+        score.user.points = 0;
+        addCircleBg("userWinShowCircleBg");
+        winnerName = "YOU WON";
+    } else if (score.user.points < score.cpu.points) {
+        score.cpu.totalScore += 1;
+        score.cpu.points = 0;
+        addCircleBg("cpuWinShowCircleBg");
+        winnerName = "YOU LOSE";
     }
+
+    showWinnerName(winnerName);
 
 }
 
-function checkIfEqual(userInput, cpuInput) {
-    
-    console.log(userInput,cpuInput);
-
+function checkDueMatch(userInput, cpuInput) {
     if (userInput === cpuInput) {
-        alert('DUE');
-    } else {    
-        check(`${userInput} > ${cpuInput}`,userInput,cpuInput)
-        check(`${cpuInput} > ${userInput}`,userInput,cpuInput)
+        showWinnerName("It's a draw");
+    } else {
+        checkRules(`${userInput} > ${cpuInput}`, userInput, cpuInput);
+        checkRules(`${cpuInput} > ${userInput}`, userInput, cpuInput);
+        declareResult();
     }
-
 }
 
-declareResult();
+function playAgain() {
+    selectUserInputElement.style.display = "flex";
+    displayResult.innerHTML = "";
+    displayResult.style.display = "none";
+    popupWrapper.style.display = 'none';
+    popupWrapper.style.opacity = 0
+}
+
+function showWinnerName(name) {
+    const declareWinnerName = document.getElementById("declareWinnerName");
+    const h1 = declareWinnerName.childNodes[1];
+    h1.style.visibility = 'visible'
+    h1.innerText = name
+
+    if (count >= 4 && score.user.totalScore > score.cpu.totalScore) {
+        userScore.innerText = 0;
+        updatePopupInfo(':)', 'Congratulations! You emerged victorious in this match! 🎉 Well done on your win!')
+        resetScore();
+    } else if (count >= 4 && score.user.totalScore < score.cpu.totalScore) {
+        userScore.innerText = 0;
+        updatePopupInfo(':(', 'Unfortunately, the game did not turn out in your favor this time. Keep your spirits up!')
+        resetScore();
+    } else {
+        count++;
+    }
+}
+
+function updatePopupInfo(title, desc) {
+    const child = popupWrapper.childNodes;
+    child[1].childNodes[1].innerText = title
+    child[1].childNodes[3].innerText = desc
+}
+
+function addCircleBg(id) {
+    document.getElementById(id).classList.add("animatedCircles");
+    document.getElementById(id).classList.remove("hide");
+}
+
+function resetScore() {
+    score = {
+        user: { name: "", points: 0, totalScore: 0 },
+        cpu: { name: "", points: 0, totalScore: 0 },
+    };
+    count = 0
+    popupWrapper.style.display = 'flex';
+    setTimeout(() => {
+        popupWrapper.style.opacity = 1
+    }, 100);
+}
